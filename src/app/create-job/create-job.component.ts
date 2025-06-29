45b0d4cd992afe877c3d07b59bf029cf4d59810b
adammfrank <adam.michael.frank@gmail.com>
2025-06-29 13:25:02 -0700

    openai parses job descriptions successfully

--- src/app/create-job/create-job.component.ts
+++ src/app/create-job/create-job.component.ts
@@ -7,7 +7,11 @@ import { MatSelectModule } from "@angular/material/select";
 import { MatRadioModule } from "@angular/material/radio";
 import { MatCardModule } from "@angular/material/card";
 
+import { z } from "zod";
+import { zodTextFormat } from "openai/helpers/zod";
+
 import OpenAI from "openai";
+import { Job } from "../../models/job.model";
 
 @Component({
   selector: "app-create-job",
@@ -32,11 +36,46 @@ export class CreateJobComponent {
       dangerouslyAllowBrowser: true,
     });
     console.log("DESCRIPTION: " + this.jobForm.value.description);
+
+    const JobDescription = z.object({
+      id: z.string(),
+      title: z.string(),
+      company: z.string(),
+      location: z.string(),
+      salaryMin: z.number(),
+      salaryMax: z.number(),
+      tags: z.array(z.string()),
+      description: z.string(),
+      recruiterNote: z.string(),
+      datePosted: z.string().transform((str) => new Date(str)),
+    });
+
     const response = await client.responses.create({
-      model: "gpt-4.1",
-      input: "Write a one-sentence bedtime story about a unicorn.",
+      model: "gpt-4o",
+      input: [
+        {
+          role: "system",
+          content:
+            `You are an expert at structured data extraction. You will be given unstructured text from a job description and you should convert it into the given structure.
+            If you cannot extract a value for any field then please put an empty string in that field.`,
+        },
+        {
+          role: "user",
+          content: this.jobForm.value.description!,
+        },
+      ],
+      text: {
+        format: zodTextFormat(
+          JobDescription,
+          "job_description",
+        ),
+      },
     });
     console.log(response);
     alert("Thanks!");
+
+    const jobData: Job = JobDescription.parse(JSON.parse(response.output_text));
+
+    console.log("JOB DATA" + JSON.stringify(jobData));
   }
 }
